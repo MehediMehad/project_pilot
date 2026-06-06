a9409735c834de90f29b88756c5f322c7ce2a9c9
MehediMehad <mdmehedihasanmehad@gmail.com>
2026-06-06 19:49:02 +0600

    refactor: clean up next.config.ts by removing commented-out code and unused eslint configuration

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,8 +1,53 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   // reactCompiler: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: "res.cloudinary.com",
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: "randomuser.me",
+//       }
+//     ]
+//   },
+
+//   experimental: {
+//     serverActions: {
+//       bodySizeLimit: "5mb",
+//     },
+//   },
+
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+
+//   // @ts-ignore
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+
+//   // Redirect root (/) to /login automatically
+//   async redirects() {
+//     return [
+//       {
+//         source: '/',
+//         destination: '/login',
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // reactCompiler: true,
   images: {
     remotePatterns: [
       {
@@ -24,11 +69,6 @@ const nextConfig: NextConfig = {
 
   typescript: {
     ignoreBuildErrors: true,
-  },
-
-  // @ts-ignore
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 
   // Redirect root (/) to /login automatically
