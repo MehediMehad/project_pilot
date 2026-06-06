6ea83a261e1874d7dea47abd2b81889b78c4d039
MehediMehad <mdmehedihasanmehad@gmail.com>
2026-06-06 19:32:48 +0600

    refactor: simplify authentication logic by removing token verification and redirect handling

--- client/src/app/(auth)/page.tsx
+++ client/src/app/(auth)/page.tsx
@@ -1,30 +1,9 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import jwt, { JwtPayload } from "jsonwebtoken";
-import { getDefaultDashboardRoute, UserRole } from "@/lib/auth/auth-utils";
+const page = () => {
+  return (
+    <div>
+      <h1>This is page component</h1>
+    </div>
+  );
+};
 
-export const dynamic = "force-dynamic";
-
-export default async function RootPage() {
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get("accessToken")?.value || null;
-
-  if (!accessToken) {
-    redirect("/login");
-  }
-
-  try {
-    const verifiedToken = jwt.verify(
-      accessToken,
-      process.env.JWT_SECRET as string
-    ) as JwtPayload;
-
-    const userRole = verifiedToken.role as UserRole;
-    redirect(getDefaultDashboardRoute(userRole));
-  } catch (error) {
-    console.error("Error verifying token in root page redirect:", error);
-    redirect("/login");
-  }
-
-  return null;
-}
+export default page;
